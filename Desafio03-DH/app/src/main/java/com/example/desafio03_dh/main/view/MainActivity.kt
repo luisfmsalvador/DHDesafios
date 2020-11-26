eb5d61146da87abd528102da5bd11e3eb869d25b
Luis Fernando Mateus Salvador <luisfernandomateussalvador@gmail.com>
2020-11-26 00:10:48 -0300

    Estrutura inicial do Desafio03

---
+++ Desafio03-DH/app/src/main/java/com/example/desafio03_dh/main/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.desafio03_dh.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.desafio03_dh.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
